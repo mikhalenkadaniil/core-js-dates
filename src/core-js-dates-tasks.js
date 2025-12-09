0115571b0f239b4073a088f314144e48a9c5d26d
mikhalenkadaniil <mikhalenka.d@gmail.com>
2025-12-09 17:03:33 +0100

    feat: resolve getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -46,8 +46,18 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const dayNames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednsday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const dayID = new Date(date).getDay();
+  return dayNames[dayID];
 }
 
 /**
